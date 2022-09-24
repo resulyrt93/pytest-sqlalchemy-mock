25da9d272f70c70be7f203f9d8f4537ce69c3329
resulyurttakalan <resul.yurttakalan@getir.com>
2022-09-25 00:58:35 +0300

    - add ModelMocker

---
+++ tests/data.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+class MockData:
+    USER_DATA = [
+        {
+            "id": 1,
+            "name": "Kevin",
+            "surname": "Malone",
+            "is_admin": False,
+            "city": "NY",
+            "join_date": datetime(2011, 7, 27, 16, 2, 8),
+        },
+        {
+            "id": 2,
+            "name": "Dwight",
+            "surname": "Schrute",
+            "is_admin": True,
+            "city": "PA",
+            "join_date": datetime(2009, 11, 20, 21, 3, 12),
+        }
+    ]
--- tests/test_pytest_sqlalchemy_mock.py
+++ tests/test_pytest_sqlalchemy_mock.py
@@ -1,2 +1,19 @@
+from .data import MockData
+
+
 def test_get_session(session):
     assert session.execute("SELECT 5").scalar() == 5
+
+
+def test_session_user_table(session):
+    assert session.execute("SELECT count(*) from user").scalar() == 0
+
+
+def test_mocked_session_user_table(mocked_session):
+    user_data = mocked_session.execute("SELECT * from user;").first()
+    raw_data = MockData.USER_DATA[0]
+    assert user_data[0] == raw_data["id"]
+    assert user_data[1] == raw_data["name"]
+    assert user_data[2] == raw_data["surname"]
+    assert user_data[3] == raw_data["is_admin"]
+    assert user_data[4] == raw_data["city"]
